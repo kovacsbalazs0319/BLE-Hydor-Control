80e7d7996a3270aee09478f6cf28ff1d8e90e5f0
kovacsbalazs0319 <kovacsbazsi0319@gmail.com>
2025-11-01 12:20:15 +0100

    modifie testing script function declarations to be complient with 1 line usage for BLE gatt event handler

--- control.h
+++ control.h
@@ -1,6 +1,21 @@
-static void flow_gpio_init(void);
-static void pump_gpio_init(void);
-static void timing_init(void);
-void app_init_once(void);
-void GPIO_EVEN_IRQHandler(void);
-void app_main_loop_step(void);
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+// Egysoros "sink" callback típus (app.c adja meg).
+// Minden új mintánál hívjuk: lpm, pulses, error_code (0=OK, !0 hiba)
+typedef void (*hydro_sink_t)(float lpm, uint32_t pulses, uint8_t error_code, void *user);
+
+// Init: GPIO + IRQ + belső állapot
+void hydro_init(void);
+
+// Pumpa engedélyezés/tiltás (elindítja/leállítja a belső mintavételt)
+void hydro_enable(bool on);
+bool hydro_is_enabled(void);
+
+// Aktuális értékek lekérdezése
+float    hydro_get_flow_lpm(void);
+uint32_t hydro_get_pulse_count(void);
+
+// App oldali "1 soros" GATT küldés beregisztrálása
+void hydro_set_sink(hydro_sink_t cb, void *user);
